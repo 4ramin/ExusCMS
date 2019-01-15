eb43246b5525bd2142c61a45856eb4bd6c5ecd76
ExuscmsPackage <43326918+ExuscmsPackage@users.noreply.github.com>
2019-01-15 15:45:48 +0900

    Master

--- common/classes/components/FileSystem/Directory.php
+++ common/classes/components/FileSystem/Directory.php
@@ -2,20 +2,25 @@
 
 	if(!defined("__FLOWER__")) exit();
 
-	class dir{
+	class dir
+	{
 	   
 		/**
 		 * get directory root
 		 */
-		public static function dir_root(){
+		public static function dir_root()
+		{
 			return dirname(__FILE__);
 		}
 		
-		public static function isEmpty($dir){
+		public static function isEmpty($dir)
+		{
 			if (!is_readable($dir)) return null;
 			$handle = opendir($dir);
-			while(($entry = readdir($handle)) !== false){
-				if($entry !== '.' && $entry !== '..'){
+			while(($entry = readdir($handle)) !== false)
+			{
+				if($entry !== '.' && $entry !== '..')
+				{
 					return false;
 				}
 			}
@@ -23,12 +28,17 @@
 			return true;
 		}
 	
-		public static function emptyFolder($dir){
+		public static function emptyFolder($dir)
+		{
 			$glob = glob($folder);
-			foreach ($glob as $g) {
-				if (!is_dir($g)) {
+			foreach ($glob as $g) 
+			{
+				if (!is_dir($g)) 
+				{
 					unlink($g);
-				} else {
+				} 
+				else 
+				{
 					delete_folder("$g/*");
 					rmdir($g);
 				}
@@ -38,7 +48,8 @@
 		/**
 		 * get document root
 		 */
-		public static function root(){
+		public static function root()
+		{
 			return $_SERVER['DOCUMENT_ROOT'];
 		}
 		
@@ -50,7 +61,8 @@
 		 *
 		 * @return pdo
 		 */
-		public static function make($args){
+		public static function make($args)
+		{
 			
 			//args
 			$path = $args->path;
@@ -61,15 +73,27 @@
 			settype($mode,"integer");
 			
 			//make single directory
-			if(!is_array($path)){
-				if(!is_dir($path)){
-					if(!mkdir($path, $mode, true)) return FALSE;
+			if(!is_array($path))
+			{
+				if(!is_dir($path))
+				{
+					if(!mkdir($path, $mode, true)) 
+					{
+						return FALSE;
+					}
 				}
-			}else{
+			}
+			else
+			{
 			//make array directory
-				foreach($path as $val){
-					if(!is_dir($val)){
-						if(!mkdir($val, $mode, true)) return FALSE;
+				foreach($path as $val)
+				{
+					if(!is_dir($val))
+					{
+						if(!mkdir($val, $mode, true)) 
+						{
+							return FALSE;
+						}
 					}
 				}
 			}
@@ -83,24 +107,34 @@
 		 *
 		 * @return pdo
 		 */
-		public static function remove($args){
+		public static function remove($args)
+		{
 			
 			//args
 			$path = $args->path;
 			
 			//remove single directory
-			if(!is_array($path)){
-				if(is_dir($path)){
-					if(!rmdir($path)) return FALSE;
+			if(!is_array($path))
+			{
+				if(is_dir($path))
+				{
+					if(!rmdir($path)) 
+					{
+						return FALSE;
+					}
 				}
 			}else{
 			//remove array directory
 				foreach($path as $val){
 					if(is_dir($val)){
-						if(!rmdir($val)) return FALSE;
+						if(!rmdir($val)) 
+						{
+							return FALSE;
+						}
 					}
 				}
 			}
+			
 			return TRUE;
 		}
 		
@@ -116,7 +150,8 @@
 		 *
 		 * return boolean
 		 */
-		public static function exist($args){
+		public static function exist($args)
+		{
 			
 			//args
 			$path = $args->path;
@@ -131,7 +166,8 @@
 		 *
 		 * @return pdo
 		 */
-		public static function scan($args){
+		public static function scan($args)
+		{
 			
 			//args
 			$path = $args->path;
@@ -149,56 +185,91 @@
 		 *
 		 * @return array
 		 */
-		public static function _list(\stdClass $args){
+		public static function _list(\stdClass $args)
+		{
 			
 			//variables
 			$arr_list = array();
 			
 			//args
 			$path = $args->path;
-			if(isset($args->target)) $list_target = $args->target;
+			if(isset($args->target)) 
+			{
+				$list_target = $args->target;
+			}
+			
 			$include_subdirectory = $args->show_sub;
-			if(!isset($include_subdirectory)) $include_subdirectory = FALSE;
+			if(!isset($include_subdirectory)) 
+			{
+				$include_subdirectory = FALSE;
+			}
+			
 			$include_path = $args->show_path;
-			if(!isset($include_path)) $include_path = FALSE;
+			if(!isset($include_path)) 
+			{
+				$include_path = FALSE;
+			}
 			
 			//get handle
 			$handle = opendir($path);
-			if(!$handle){
+			if(!$handle)
+			{
 				return FALSE;
 			}
 			
 			//scan
-			if($include_subdirectory == FALSE){
-				while(false !== ($file = readdir($handle))){
-					if(!isset($list_target)){
-						if($include_path == TRUE){
+			if($include_subdirectory == FALSE)
+			{
+				while(false !== ($file = readdir($handle)))
+				{
+					if(!isset($list_target))
+					{
+						if($include_path == TRUE)
+						{
 							array_push($arr_list,$path.'/'.$file);
-						}else{
-							array_push($arr_list,$file);
 						}
-					}elseif(strtoupper($list_target) == 'FILE' && is_file($path.'/'.$file)){
-						if($include_path == TRUE){
-							array_push($arr_list,$path.'/'.$file);
-						}else{
-							array_push($arr_list,$file);
-						}
-					}elseif(strtoupper($list_target) == 'PATH' && is_dir($path.'/'.$file)){
-						if($include_path == TRUE){
-							array_push($arr_list,$path.'/'.$file);
-						}else{
+						else
+						{
 							array_push($arr_list,$file);
 						}
 					}
+					elseif(strtoupper($list_target) == 'FILE' && is_file($path.'/'.$file))
+					{
+						if($include_path == TRUE)
+						{
+							array_push($arr_list,$path.'/'.$file);
+						}
+						else
+						{
+							array_push($arr_list,$file);
+						}
+					}
+					elseif(strtoupper($list_target) == 'PATH' && is_dir($path.'/'.$file))
+					{
+						if($include_path == TRUE)
+						{
+							array_push($arr_list, $path.'/'.$file);
+						}
+						else
+						{
+							array_push($arr_list, $file);
+						}
+					}
 				}
-			}else{
+			}
+			else
+			{
 				//scan
 				$di = new RecursiveDirectoryIterator($path);
-				foreach (new RecursiveIteratorIterator($di) as $filename => $file){
-					if($include_path == TRUE){
-						array_push($arr_list,$filename);
-					}else{
-						array_push($arr_list,$file->getFilename());
+				foreach (new RecursiveIteratorIterator($di) as $filename => $file)
+				{
+					if($include_path == TRUE)
+					{
+						array_push($arr_list, $filename);
+					}
+					else
+					{
+						array_push($arr_list, $file->getFilename());
 					}
 				}
 			}
