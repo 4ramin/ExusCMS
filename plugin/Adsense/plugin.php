d9610d27e8d93c8b2ab0bafb94fafe78458ec73c
ExuscmsPackage <43326918+ExuscmsPackage@users.noreply.github.com>
2019-01-28 09:48:06 +0900

    Master

--- plugin/Adsense/plugin.php
+++ plugin/Adsense/plugin.php
@@ -7,7 +7,7 @@
 		$this->base = new base();
 		$output = $this->base->get('content');
 
-		$output = str_replace('<div style="" id="rel_ar', '<img src="https://ncache.ilbe.com/files/attach/new/20151012/28622079/2871294061/6737531752/e1ec3bbda91db28ef5de341330d40b50.jpg"/>'.'<div style="" id="rel_ar', $output);
+		$output = str_replace('<div style="" id="rel_ar', '<p>test</p>'.'<div style="" id="rel_ar', $output);
 		$this->base->set('content', $output);
 	}
 	
