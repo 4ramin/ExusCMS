6856f096e1f638257a74745421f97403fbe3bb27
ExuscmsPackage <43326918+ExuscmsPackage@users.noreply.github.com>
2019-01-29 15:39:03 +0900

    Pagenation annotation

--- common/classes/components/Request/Pagenation.php
+++ common/classes/components/Request/Pagenation.php
@@ -1,17 +1,28 @@
 <?php
-
+/**
+ * @class PageNation
+ *
+ * @Generate Pagenation
+ */
 class Pagenation
 {
 	
-	var $point;
-	var $page_margin;
-	var $first_page = 0;
-	var $total_count;
+	var $point;           // goted page count
+	var $page_margin = 0; // page margin for center align
+	var $first_page = 0;  // first page number
+	var $total_count;     // number of total items
 	
+	/**
+	 * Constructor
+	 *
+	 * @param int $total_count  : number of total items
+	 * @param int $current_page : current page number
+	 * @param int $list_count   : number of page links displayed at one time
+	 *	 
+	 * @return void
+	 */
 	function __construct($total_count, $current_page = 1, $list_count = 10) 
 	{
-		$page_margin = 0;
-		$first_page = 0;
 		$list_count_rel = ceil($list_count / 2);
 		$total_count = ($total_count < 0) ? 1 : $total_count;
 		
@@ -38,46 +49,80 @@ class Pagenation
 		$this->total_count = $total_count;
 		$this->current_page = $current_page;
 		$this->list_count = $list_count;
-		echo print_r($this)."/".$current_page."/".($total_count - ($list_count - 1));
-		
 	}
 	
+	/**
+	 * get a last page.
+	 *
+	 * @return int
+	 */
 	function getLastPage()
 	{
 		return $this->board->page_count;
 	}
 	
+	/**
+	 * get a link of last page.
+	 *
+	 * @return String
+	 */
 	function getLastPageLink()
 	{
 		return str::getUrl(__MODULEID, $_GET[__MODULEID], 'page', $this->getLastPage(), 'srl', '');
 	}
 	
+	/**
+	 * get a comment link of last page.
+	 *
+	 * @return String
+	 */
 	function getCommentPageLink()
 	{
 		return str::getUrl(__MODULEID, $_GET[__MODULEID], 'cpage', $value, 'act', 'getCommentPage');
 	}
 	
+	/**
+	 * get a page link.
+	 *
+	 * @return String
+	 */
 	function getPageLink()
 	{
 		return str::getUrl(__MODULEID, $_GET[__MODULEID], 'page', $this->getCurrentPage(), 'srl', '');
 	}
 	
+	/**
+	 * make sure this page is the same as the current page.
+	 *
+	 * @return boolean
+	 */
 	function isCurrentPage()
 	{
 		return ($_GET['page'] == $this->getCurrentPage()) ? true : false;
 	}
 	
+	/**
+	 * make sure this comment page is the same as the current page.
+	 *
+	 * @return boolean
+	 */
 	function isCurrentCPage()
 	{
 		return ($_GET['cpage'] == $this->getCurrentPage()) ? true : false;
 	}
 	
+	/**
+	 * make sure has next page.
+	 *
+	 * @return boolean
+	 */
 	function hasNextPage()
 	{
-		$page = ($this->first_page + ++$this->point);
+		$page = $this->first_page + ++$this->point;
 		if ($page > $this->list_count)
 		{
 			$this->point = 0;
+			
 			return false;
 		}
 		else
@@ -86,6 +131,11 @@ class Pagenation
 		}
 	}
 	
+	/**
+	 * get a current page.
+	 *
+	 * @return int
+	 */
 	function getCurrentPage()
 	{
 		return ($this->page_margin + $this->first_page + $this->point);
